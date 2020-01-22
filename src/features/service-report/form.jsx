3b1a94b5cb53f66e926708a597027ac790b2ea84
artinsepanta <faribasalehi@hotmail.com>
2020-01-22 16:53:01 -0600

    work on form

--- src/features/service-report/form.jsx
+++ src/features/service-report/form.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import {Field, reduxForm} from 'redux-form'
 
 function validate(values) {
   const errors ={}
-  const{ no } = values
+  const{ no, type, description, process } = values
   if(!no) errors.no = "required"
+  if(!type) errors.type = "required"
+  if(!description) errors.description = "required"
+  if(!process) errors.process = "required"
 
   return errors
 }
@@ -20,14 +23,39 @@ function renderInput({input, lable,type,meta:{touched,error}}) {
   </div>
 }
 
+function renderTextArea({input, lable, rows, meta: {touched, error}}) {
+  return <div>
+    <lable>{lable}{touched && ((error && <span className='error'>{error}</span>))}</lable>
+    <div>
+      <textarea {...input } placeholder={lable} rows={rows}/>
+    </div>
+  </div>
+}
+
 function SerReportForm(props) {
-  const { handleSubmit, submitting,valid} = this.props
+  const { handleSubmit, submitting,valid} = props
   return <form onSubmit={handleSubmit}>
     <Field name="no" component={renderInput} type="text" lable="No."/>
+    <Field name="tupe" component={renderInput} type="text" lable="Type"/>
+    <Field name="description" component={renderTextArea} rows= {10} lable="Description"/>
+    <Field name="process" component={renderTextArea} rows= {10} lable="Process"/>
+   
+    <div>
+      <button 
+        type="submit"
+        className="primary"
+        disabled = {!valid ||submitting}
+      >Save</button>
+      <button
+        type="button"
+        className="secondary"
+        onClick={() => props.history.push('/')}
+      >Cancel</button>
+    </div>
   </form>
 }
 
-const withForm = reduxForm({ form:'serReport', validate})(SerReportForm)
+const withForm = reduxForm({ form: 'serReport', validate})(SerReportForm)
 const withRedux = connect()(withForm)
 
-export default withRedux
+export default(withRedux) 
